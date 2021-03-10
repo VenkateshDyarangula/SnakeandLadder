acc0c90adfa2cc0c58ef42c9dd04191b45744443
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-10 20:25:17 +0530

    added

---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLagger
+{
+    class Player
+    {
+        public static  int ENDPOSITION = 100;
+        private int position;
+        public Player()
+        {
+            position = 0;
+        }
+        public int getPosition()
+        {
+            return position;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿ using System;
+
+namespace SnakeLagger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p=new Player();
+            Console.WriteLine("Initial Position = " + p.getPosition());
+        }
+    }
+}
