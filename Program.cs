a77b9afdc845e958d603a7a19263e43f7ead1661
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-10 21:20:03 +0530

    UC4 added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-#UC2
+#UC1
  using System;
 
 namespace SnakeLagger
@@ -15,10 +15,37 @@ namespace SnakeLagger
 
 
 
+
+#UC2
+using System;
+
+namespace CheckOpt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Giveopt give = new Giveopt();
+            int count=give.throwDie();
+            int option=give.giveOption(count);
+            if (option == 0)
+                Console.WriteLine("Player stays in the same position "+option);
+            else if (option == count)
+                Console.WriteLine("Ladder: Player Moves ahead of diecount " +count);
+            else if (option == (-1)*count)
+
+                Console.WriteLine("Snake: Player Movesdown of diecount " +option);
+        }
+
+    }
+}
+
+
+
 #UC3
 using System;
 
-namespace UC2_RollDie
+namespace UC3_RollDie
 {
     class Program
     {
